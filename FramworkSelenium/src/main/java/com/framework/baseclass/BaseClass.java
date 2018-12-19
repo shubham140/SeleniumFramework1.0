0b04d83b47919a5e95b91e3a9f9e3b70060db2b7
Shubham <Shubham@192.168.1.11>
2018-12-19 07:07:52 +0530

    NewCommit

--- FramworkSelenium/src/main/java/com/framework/baseclass/BaseClass.java
+++ FramworkSelenium/src/main/java/com/framework/baseclass/BaseClass.java
@@ -3,10 +3,19 @@
  */
 package com.framework.baseclass;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
 /**
  * @author Shubham
  *
  */
 public class BaseClass {
+	
+	public void initializeDriver() throws Exception {
+	Properties property=new Properties();
+	FileInputStream fis=new FileInputStream("");
 
+}
 }
